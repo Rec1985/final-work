99d3b6933f8319aa7da121e820b511ba0c77fe9f
Vladimir Mironov <rec11-07-1985@mail.ru>
2023-02-28 11:28:00 +0300

    done programm

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] originalArray = new string[] {"hello", "2", "world", ":-)"};
+string[] resultArray = new string[originalArray.Length];
+int sizeString = 3;
+
+TransformationArray(originalArray, resultArray, sizeString);
+PrintArray(resultArray);
+
+void TransformationArray(string[] arr1, string[] arr2, int size)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[i].Length <= size)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
